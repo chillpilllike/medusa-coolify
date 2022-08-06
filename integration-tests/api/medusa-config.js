9f497b6fa2d4537f07d3b88f6d230d0e0695e45e
chillpilllike <34432585+chillpilllike@users.noreply.github.com>
2022-08-07 00:23:13 +0530

    Update medusa-config.js

--- integration-tests/api/medusa-config.js
+++ integration-tests/api/medusa-config.js
@@ -1,3 +1,28 @@
+const dotenv = require('dotenv')
+
+ let ENV_FILE_NAME = '';
+ switch (process.env.NODE_ENV) {
+  case 'production':
+    ENV_FILE_NAME = '.env.production';
+    break;
+  case 'staging':
+    ENV_FILE_NAME = '.env.staging';
+    break;
+  case 'test':
+    ENV_FILE_NAME = '.env.test';
+    break;
+  case 'development':
+  default:
+    ENV_FILE_NAME = '.env';
+    break;
+ }
+
+ try {
+  dotenv.config({ path: process.cwd() + '/' + ENV_FILE_NAME });
+ } catch (e) {
+  //handle error
+ }
+
 const DB_USERNAME = process.env.DB_USERNAME || "postgres"
 const DB_PASSWORD = process.env.DB_PASSWORD || ""
 const workerId = parseInt(process.env.JEST_WORKER_ID || "1")
